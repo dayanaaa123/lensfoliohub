ac88e7e427496eaa8d3b0016965ac4299df80a46
Ivan <84211523+ivan-the-noob@users.noreply.github.com>
2025-01-11 06:53:00 -0800

    Update db.php

--- db/db.php
+++ db/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";  
-$username = "root";         
-$password = "";             
+$username = "diana";         
+$password = "lens";             
 $dbname = "LENSFOLIOHUB";   
 
 $conn = new mysqli($servername, $username, $password, $dbname);
