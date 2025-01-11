ae3caa74bd5237fecd3f2359c6b69c0781de3b18
dayanaaa123 <dianamae.bustamante@cvsu.edu.ph>
2025-01-11 09:52:58 -0800

    Jan 11

--- features/clients/web/api/notifications.php
+++ features/clients/web/api/notifications.php
@@ -50,7 +50,10 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
+    <link rel="icon" href="assets/img/profile.jpg" type="image/png">
+    <title>LENSFOLIOHUB</title>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../../css/status.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
--- features/suppliers/web/api/snapfeeds.php
+++ features/suppliers/web/api/snapfeeds.php
@@ -28,7 +28,10 @@ if ($role != 'guest' && !empty($email)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
+    <link rel="icon" href="assets/img/profile.jpg" type="image/png">
+    <title>LENSFOLIOHUB</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
